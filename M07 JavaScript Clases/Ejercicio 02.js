378e1af245f120c66953d8e795935933b2706125
biancazcalderoni <biancazcalderoni@gmail.com>
2023-08-10 21:00:05 -0300

    Módulo 07 completado.

--- M07 JavaScript Clases/Ejercicio 02.js
+++ M07 JavaScript Clases/Ejercicio 02.js
@@ -38,8 +38,8 @@ function agregarMetodo() {
    // Este método toma la propiedad "nombre" y "edad", y devuelve el string:
    // Ejemplo: "Juan, 22 años".
    // Tu código:
-   Persona.prototype.datos = () => {
-      return this.nombre + ", " + this.edad + "años";
+   Persona.prototype.datos = function() {
+      return this.nombre + ", " + this.edad + " años";
    }
 }
 
